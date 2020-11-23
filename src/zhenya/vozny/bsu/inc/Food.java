6862b318c879ed02dc8d618b6aed881740c9edd8
zhenekq <zhenek02ss@gmail.com>
2020-11-23 22:46:41 +0300

    completed method Food

--- src/zhenya/vozny/bsu/inc/Food.java
+++ src/zhenya/vozny/bsu/inc/Food.java
@@ -7,7 +7,23 @@ public abstract class Food implements Consumable {
         return name;
     }
 
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object arg0) {
+        if(!(arg0 instanceof Food )) return false;
+        if(name == null || ((Food)arg0).name == null) return false;
+        return name.equals(((Food)arg0).name);
+    }
+
     public void setName(String name){
+        this.name = name;
+    }
+
+    public Food(String name){
         this.name = name;
     }
 
