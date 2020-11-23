944fea8f7abc56e4b20bc1a72ef8e5378fa13afe
zhenekq <zhenek02ss@gmail.com>
2020-11-23 22:40:15 +0300

    make getters and setters

--- src/zhenya/vozny/bsu/inc/Food.java
+++ src/zhenya/vozny/bsu/inc/Food.java
@@ -1,6 +1,15 @@
 package zhenya.vozny.bsu.inc;
 
 public abstract class Food implements Consumable {
+    String name = null;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 
     @Override
     public void consume() {
